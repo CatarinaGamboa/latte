9123d315aa080448d015a8ee45ba2ee4ff0f9f17
kiranraoboinapally <kiranraoboinapally@gmail.com>
2025-04-21 15:43:50 +0530

    t rebase --continue mine

--- latte/src/test/java/AppTest.java
+++ latte/src/test/java/AppTest.java
@@ -63,65 +63,122 @@ public class AppTest {
      */
 
     @Test
-    public void testMyNode() {
-        assertLauncherFails("src/test/examples/MyNode.java", "UNIQUE but got BORROWED");
+    public void testMyNode(){
+        try {
+            App.launcher("src/test/examples/MyNode.java");
+        } catch (Exception e) {
+            assertTrue(e instanceof LatteException);
+            assertTrue(e.getMessage().contains("UNIQUE but got BORROWED"));
+        }
+        
     }
 
-    @Test
-    public void testMyNodePushPopIncorrect() {
-        assertLauncherFails("src/test/examples/MyNodePushPopIncorrect.java", "FREE but got BOTTOM");
-    }
+   @Test
+   public void testMyNodePushPopIncorrect(){
+       try {
+           App.launcher("src/test/examples/MyNodePushPopIncorrect.java");
+       } catch (Exception e) {
+           System.out.println(e.getMessage());
+           assertTrue(e instanceof LatteException);
+           assertTrue(e.getMessage().contains("FREE but got BOTTOM"));
+       }
+       
+   }
 
-    @Test
-    public void testMyNodeNoDistinct() {
-        assertLauncherFails("src/test/examples/MyNodeNoDistinct.java", "Non-distinct parameters");
-    }
+   @Test
+   public void testMyNodeNoDistinct(){
+       try {
+           App.launcher("src/test/examples/MyNodeNoDistinct.java");
+       } catch (Exception e) {
+           assertTrue(e instanceof LatteException);
+           assertTrue(e.getMessage().contains("Non-distinct parameters"));
+       }   
+   }
 
-    @Test
-    public void testMyNodeCallUniqueFree() {
-        assertLauncherFails("src/test/examples/MyNodeCallUniqueFree.java", "FREE but got UNIQUE");
-    }
+   @Test
+   public void testMyNodeCallUniqueFree(){
+       try {
+           App.launcher("src/test/examples/MyNodeCallUniqueFree.java");
+       } catch (Exception e) {
+           assertTrue(e instanceof LatteException);
+           assertTrue(e.getMessage().contains("FREE but got UNIQUE"));
+       }
+       
+   }
 
-    @Test
-    public void testSmallestIncorrectExample() {
-        assertLauncherFails("src/test/examples/SmallestIncorrectExample.java", "UNIQUE but got BORROWED");
-    }
+   @Test
+   public void testSmallestIncorrectExample(){
+       try {
+           App.launcher("src/test/examples/SmallestIncorrectExample.java");
+       } catch (Exception e) {
+           assertTrue(e instanceof LatteException);
+           assertTrue(e.getMessage().contains("UNIQUE but got BORROWED"));
+       }
+       
+   }
 
-    @Test
-    public void testMyStackFieldAssignMethod() {
-        assertLauncherFails("src/test/examples/MyStackFieldAssignMethod.java", "UNIQUE but got SHARED");
-    }
+   @Test
+   public void testMyStackFieldAssignMethod(){
+       try {
+           App.launcher("src/test/examples/MyStackFieldAssignMethod.java");
+       } catch (Exception e) {
+           assertTrue(e instanceof LatteException);
+           assertTrue(e.getMessage().contains("UNIQUE but got SHARED"));
+       }
+   }
 
-    @Test
-    public void testFieldAccessNoThis() {
-        assertLauncherFails("src/test/examples/FieldAccessNoThis.java", "UNIQUE but got SHARED");
-    }
+   @Test
+   public void testFieldAccessNoThis(){
+       try {
+           App.launcher("src/test/examples/FieldAccessNoThis.java");
+       } catch (Exception e) {
+           assertTrue(e instanceof LatteException);
+           assertTrue(e.getMessage().contains("UNIQUE but got SHARED"));
+       }
+   }
 
-    @Test
-    public void testFieldAccessRightNoThis() {
-        assertLauncherFails("src/test/examples/FieldAccessRightNoThis.java", "FREE but got UNIQUE");
-    }
+   @Test
+   public void testFieldAccessRightNoThis(){
+       try {
+           App.launcher("src/test/examples/FieldAccessRightNoThis.java");
+       } catch (Exception e) {
+           assertTrue(e instanceof LatteException);
+           assertTrue(e.getMessage().contains("FREE but got UNIQUE"));
+       }
+   }
 
-    /*
-     * === Reachability Unit Test ===
-     */
 
-    @Test
-    public void testReachabilityUnitTest() {
-        Logger logger = Logger.getLogger(AppTest.class.getName());
+   @Test
+   public void testReachabilityUnitTest(){
+       Logger logger = Logger.getLogger(AppTest.class.getName());
+       //test
+       SymbolicEnvironment se = new SymbolicEnvironment();
+       se.enterScope();
+       SymbolicValue v1 = se.addVariable("x");
+       // x->1
+       SymbolicValue v2 = se.addVariable("y");
+       // x->1; y->2
+       SymbolicValue v3 = se.addField(v1,"f");
+       // x->1; y->2, 1.f->3
+       se.addVarSymbolicValue("z", v1);
+       SymbolicValue v4 = se.get("z");
+       // x->1; y->2, 1.f->3, z -> 1
 
-        SymbolicEnvironment se = new SymbolicEnvironment();
-        se.enterScope();
-        SymbolicValue v1 = se.addVariable("x");
-        SymbolicValue v2 = se.addVariable("y");
-        SymbolicValue v3 = se.addField(v1, "f");
-        se.addVarSymbolicValue("z", v1);
-        SymbolicValue v4 = se.get("z");
+       logger.info(se.toString());
 
-        logger.info(se.toString());
+       boolean b = se.canReach(v1, v2, new ArrayList<>());
+       logger.info(v1.toString() + " can reach " +  v2.toString() + "? " + b);
+       assertFalse(b);
+       
+       boolean b1 = se.canReach(v1, v3, new ArrayList<>());
+       logger.info(v1.toString() + " can reach " +  v3.toString() + "? " + b1);
+       assertTrue(b1);
 
-        assertFalse(se.canReach(v1, v2, new ArrayList<>()));
-        assertTrue(se.canReach(v1, v3, new ArrayList<>()));
-        assertTrue(se.canReach(v1, v4, new ArrayList<>()));
-    }
+       
+       boolean b2 = se.canReach(v1, v4, new ArrayList<>());
+       logger.info(v1.toString() + " can reach " +  v4.toString() + "? " + b1);
+       assertTrue(b2);
+   }
+
+
 }
